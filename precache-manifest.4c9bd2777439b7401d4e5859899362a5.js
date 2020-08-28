71905294ff4949990f71aa29e343432bffac32d1
began91 <began91@yahoo.com>
2020-08-28 16:46:38 -0500

    Updates

---
+++ precache-manifest.4c9bd2777439b7401d4e5859899362a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bf782f0195e7241934ba67e036f6be",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "17c640bdb4126aa34c91",
+    "url": "/react-calc/static/css/main.bb8fd3e7.chunk.css"
+  },
+  {
+    "revision": "39a5a82cb5015de2a03b",
+    "url": "/react-calc/static/js/2.431b3d5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-calc/static/js/2.431b3d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c640bdb4126aa34c91",
+    "url": "/react-calc/static/js/main.ef9fccfe.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  }
+]);
